0605a566e3b67f4d8a6179e53b74249c0e9c7570
unknown <mattia.r86@gmail.com>
2021-01-16 21:56:50 +0100

    Remove table creation

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 auth.login_manager.init_app(app)
 
