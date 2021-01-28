546ca275e5d2ba91429841f672c23bfc7980d75d
unknown <mattia.r86@gmail.com>
2021-01-28 22:21:23 +0100

    Translate to Italian

--- forms.py
+++ forms.py
@@ -7,8 +7,8 @@ from wtforms.validators import DataRequired, Email, Optional
 class RegisterForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign me up!")
+    name = StringField("Nome", validators=[DataRequired()])
+    submit = SubmitField("Iscrivimi!")
 
 
 class LoginForm(FlaskForm):
@@ -18,35 +18,35 @@ class LoginForm(FlaskForm):
 
 
 class CreateCustomer(FlaskForm):
-    name = StringField("Customer Name", validators=[DataRequired()])
-    phone_number = TelField("Phone Number", validators=[Optional()])
+    name = StringField("Nome del Cliente", validators=[DataRequired()])
+    phone_number = TelField("Numero di Telefono", validators=[Optional()])
     email = EmailField("Email", validators=[Optional(), Email()])
-    address = StringField("Address", validators=[Optional()])
-    zip_code = StringField("ZIP Code", validators=[Optional()])
-    iva_code = StringField("IVA Code", validators=[Optional()])
-    submit = SubmitField("Submit")
+    address = StringField("Indirizzo", validators=[Optional()])
+    zip_code = StringField("Codice CAP", validators=[Optional()])
+    iva_code = StringField("Codice IVA/Partita IVA", validators=[Optional()])
+    submit = SubmitField("Invia")
 
 
 class CreateVendor(FlaskForm):
-    name = StringField("Vendor Name", validators=[DataRequired()])
-    phone_number = TelField("Phone Number", validators=[Optional()])
+    name = StringField("Nome del Fornitore", validators=[DataRequired()])
+    phone_number = TelField("Numero di Telefono", validators=[Optional()])
     email = EmailField("Email", validators=[Optional(), Email()])
-    address = StringField("Address", validators=[Optional()])
-    zip_code = StringField("ZIP Code", validators=[Optional()])
-    iva_code = StringField("IVA Code", validators=[Optional()])
-    submit = SubmitField("Submit")
+    address = StringField("Indirizzo", validators=[Optional()])
+    zip_code = StringField("Codice CAP", validators=[Optional()])
+    iva_code = StringField("Codice IVA/Partita IVA", validators=[Optional()])
+    submit = SubmitField("Invia")
 
 
 class CreateLine(FlaskForm):
-    line_date = DateField("Date", validators=[DataRequired()])
-    kind = SelectField("Kind", choices=["Invoice", "Expense"], validators=[DataRequired()])
-    item = StringField("Description", validators=[DataRequired()])
-    currency = SelectField("Currency", choices=['CHF', 'EUR', 'USD'])
-    amount = StringField("Amount", validators=[DataRequired()])
-    paid = BooleanField("Paid?")
-    customer = SelectField("Customer")
-    vendor = SelectField("Vendor")
-    document = FileField("Invoice or receipt")
-    submit = SubmitField("Submit")
+    line_date = DateField("Data", validators=[DataRequired()])
+    kind = SelectField("Tipologia", choices=["Invoice", "Expense"], validators=[DataRequired()])
+    item = StringField("Descrizione", validators=[DataRequired()])
+    currency = SelectField("Valuta", choices=['CHF', 'EUR', 'USD'])
+    amount = StringField("Ammontare", validators=[DataRequired()])
+    paid = BooleanField("Pagata?")
+    customer = SelectField("Cliente")
+    vendor = SelectField("Fornitore")
+    document = FileField("Fattura o ricevuta")
+    submit = SubmitField("Invia")
 
 
